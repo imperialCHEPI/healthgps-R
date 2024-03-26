8c9fdcc29846530bb5f68d6974eae33e49aed695
Jack Olney <jack.olney@me.com>
2024-03-26 10:15:08 +0000

    initial commit

---
+++ run.R
@@ -0,0 +1,12 @@
+# Starting point for Health-GPS implementation in R.
+# What interesting questions can we ask and answer here.
+
+a <- 2
+a
+
+a <- rnorm(1e5, mean = 5, sd = 0.1)
+
+plot(a)
+
+tail(a)
+
